4b70c3ebf6ff2463f0f2a6ec9c9f3d42f1bdca1c
unknown <oscar.pineros-c@labinfo.is.escuelaing.edu.co>
2022-06-07 12:27:05 -0500

    El programa recibe un archivo e imprime el numero de lineas

--- src/main/java/edu/escuelaing/arem/ASE/app/App.java
+++ src/main/java/edu/escuelaing/arem/ASE/app/App.java
@@ -1,5 +1,7 @@
 package edu.escuelaing.arem.ASE.app;
 
+import java.io.*;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,20 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //System.out.println( "Hello World! " + args[0] + "file: " + args[1]);
+        int contador = 0;
+        String archivoEntrada = args[1];
+        try {
+            FileReader lectorArchivo = new FileReader(archivoEntrada);
+            BufferedReader lectorLineas = new BufferedReader(lectorArchivo);
+            while (lectorLineas.readLine() != null){
+                contador++;
+            }
+            System.out.println(contador);
+            } catch (FileNotFoundException ex) {
+            throw new RuntimeException(ex);
+            } catch (IOException e) {
+            System.out.println(e);
+        }
     }
 }
